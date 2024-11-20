eb3329dfc6f876d6416235ff2c11dc17d96b766f
marconiraw <marikkostyuk1@gmail.com>
2024-11-20 11:47:26 +0200

    edited build.proj file and add input files

---
+++ Lab3/Lab3.Tests/Tests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using lab3;
+
+namespace lab3.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestCase1()
+        {
+            string input = "2 2\n1 2 3\n1 2 4";
+            string expectedOutput = "3 1\n1";
+
+            RunTest(input, expectedOutput);
+        }
+
+        [TestMethod]
+        public void TestCase2()
+        {
+            string input = "3 3\n1 2 5\n1 3 10\n3 2 4";
+            string expectedOutput = "14 2\n2 3";
+
+            RunTest(input, expectedOutput);
+        }
+
+        [TestMethod]
+        public void TestCase3()
+        {
+            string input = "4 4\n1 2 5\n2 3 5\n3 4 5\n4 2 5";
+            string expectedOutput = "15 3\n1 2 3";
+
+            RunTest(input, expectedOutput);
+        }
+
+        [TestMethod]
+        public void TestCase4()
+        {
+            string input = "3 2\n1 2 0\n2 3 0";
+            string expectedOutput = "0 2\n1 2";
+
+            RunTest(input, expectedOutput);
+        }
+
+        [TestMethod]
+        public void TestCase5()
+        {
+            string input = "5 5\n1 2 1\n2 3 2\n3 4 3\n4 5 4\n5 1 5";
+            string expectedOutput = "10 4\n1 2 3 4";
+
+            RunTest(input, expectedOutput);
+        }
+
+        private void RunTest(string input, string expectedOutput)
+        {
+            // Путь к директории lab3
+            string lab3Directory = Path.Combine("..", "..", "lab3");
+
+            // Запись input.txt в директорию lab3
+            File.WriteAllText(Path.Combine(lab3Directory, "input.txt"), input);
+
+            // Запуск основного приложения
+            Program.Main(new string[0]);
+
+            // Чтение output.txt из директории lab3
+            string output = File.ReadAllText(Path.Combine(lab3Directory, "output.txt")).Trim();
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+    }
+}
---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace lab3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var edges = new List<Edge>();
+            int n = 0, m = 0;
+
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                var firstLine = sr.ReadLine().Split();
+                n = int.Parse(firstLine[0]);
+                m = int.Parse(firstLine[1]);
+
+                for (int i = 0; i < m; i++)
+                {
+                    var line = sr.ReadLine().Split();
+                    int u = int.Parse(line[0]);
+                    int v = int.Parse(line[1]);
+                    int c = int.Parse(line[2]);
+
+                    edges.Add(new Edge(u, v, c, i + 1));
+                }
+            }
+
+            int minCost = int.MaxValue;
+            List<int> minEdges = null;
+
+            int totalSubsets = 1 << m;
+
+            for (int mask = 1; mask < totalSubsets; mask++)
+            {
+                var subsetEdges = new List<Edge>();
+                int cost = 0;
+
+                for (int i = 0; i < m; i++)
+                {
+                    if ((mask & (1 << i)) != 0)
+                    {
+                        subsetEdges.Add(edges[i]);
+                        cost += edges[i].Cost;
+                    }
+                }
+
+                if (cost > minCost)
+                    continue;
+
+                if (IsAllReachable(n, subsetEdges))
+                {
+                    if (cost < minCost || (cost == minCost && subsetEdges.Count < minEdges.Count))
+                    {
+                        minCost = cost;
+                        minEdges = new List<int>();
+                        foreach (var edge in subsetEdges)
+                        {
+                            minEdges.Add(edge.Index);
+                        }
+                    }
+                }
+            }
+
+            using (StreamWriter sw = new StreamWriter("output.txt"))
+            {
+                if (minEdges != null)
+                {
+                    sw.WriteLine($"{minCost} {minEdges.Count}");
+                    minEdges.Sort();
+                    sw.WriteLine(string.Join(" ", minEdges));
+                }
+                else
+                {
+                    sw.WriteLine("0 0");
+                }
+            }
+        }
+
+        public static bool IsAllReachable(int n, List<Edge> edges)
+        {
+            var adj = new Dictionary<int, List<int>>();
+
+            for (int i = 1; i <= n; i++)
+                adj[i] = new List<int>();
+
+            foreach (var edge in edges)
+            {
+                adj[edge.From].Add(edge.To);
+            }
+
+            var visited = new HashSet<int>();
+            var stack = new Stack<int>();
+            stack.Push(1);
+            visited.Add(1);
+
+            while (stack.Count > 0)
+            {
+                int node = stack.Pop();
+                foreach (var neighbor in adj[node])
+                {
+                    if (!visited.Contains(neighbor))
+                    {
+                        visited.Add(neighbor);
+                        stack.Push(neighbor);
+                    }
+                }
+            }
+
+            return visited.Count == n;
+        }
+    }
+}
